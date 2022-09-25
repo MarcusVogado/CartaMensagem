30901eee5e4194ed1573db0f8b09b1789242f846
marcus vogado <marcus_620@hotmail.com>
2022-09-25 00:54:21 -0300

    update

--- Js/Script.js
+++ Js/Script.js
@@ -1,6 +1,19 @@
 // JavaScript source code
 document.addEventListener("DOMContentLoaded", () => {
 
+    
+
+
 });
 
 
+window.oninput = function(event){
+   
+   var campo = event.target.id; // pega o id do campo que chamou o evento
+   
+   if(campo == "_userName"){
+      document.querySelector('#a-1').value = document.getElementById('_userName').value;
+   }else if(campo == "_userSobrenome"){
+      document.querySelector('#a').value = document.getElementById('_userSobrenome').value;
+   }
+};
